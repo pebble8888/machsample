e437f7758a78098109f2c5534009c2491e80bd6c
pebble8888 <pebble8888@gmail.com>
2016-06-11 11:22:44 +0900

    mach sample for OSX10.11

---
+++ simple_ipc_client.cpp
@@ -0,0 +1,79 @@
+//
+//  simple_ipc_client.cpp
+//
+//  Copyright © 2016年 pebble8888. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "simple_ipc_common.h"
+
+int main(int argc, char **argv) {
+    kern_return_t kr;
+    msg_format_recv_t recv_msg;
+    msg_format_send_t send_msg;
+    mach_msg_header_t *recv_hdr, *send_hdr;
+    mach_port_t client_port, server_port;
+    
+    kr = bootstrap_look_up(bootstrap_port, SERVICE_NAME, &server_port);
+    EXIT_ON_MACH_ERROR("bootstrap_look_up", kr, BOOTSTRAP_SUCCESS);
+    
+    kr = mach_port_allocate(mach_task_self(),
+                            MACH_PORT_RIGHT_RECEIVE,
+                            &client_port);
+    EXIT_ON_MACH_ERROR("mach_port_allocate", kr, KERN_SUCCESS);
+    
+    printf("client_port = %d, servier_port = %d\n", client_port, server_port);
+    
+    // prepare request
+    send_hdr = &(send_msg.header);
+    send_hdr->msgh_bits = MACH_MSGH_BITS(MACH_MSG_TYPE_COPY_SEND, MACH_MSG_TYPE_MAKE_SEND);
+    send_hdr->msgh_size = sizeof(send_msg);
+    send_hdr->msgh_remote_port = server_port; // 送信先ポート
+    send_hdr->msgh_local_port = client_port;  // 送信元ポート
+    send_hdr->msgh_reserved = 0;
+    send_hdr->msgh_id = DEFAULT_MSG_ID;
+    send_msg.data = 0;
+    
+    if (argc == 2)
+        send_msg.data = atoi(argv[1]);
+    if ((send_msg.data < 1) || (send_msg.data > 20))
+        send_msg.data = 4;
+    
+    // send request
+    kr = mach_msg(send_hdr,
+                  MACH_SEND_MSG,
+                  send_hdr->msgh_size,
+                  0,
+                  MACH_PORT_NULL,
+                  MACH_MSG_TIMEOUT_NONE,
+                  MACH_PORT_NULL);
+    EXIT_ON_MACH_ERROR("mach_msg(send)", kr, MACH_MSG_SUCCESS);
+    
+    do {
+        // receive reply
+        recv_hdr = &(recv_msg.header);
+        recv_hdr->msgh_remote_port = server_port;
+        recv_hdr->msgh_local_port = client_port;
+        recv_hdr->msgh_size = sizeof(recv_msg);
+        
+        // クラッシュをシュミレート
+        {
+            printf("crash!\n");
+            exit(3);
+        }
+        
+        kr = mach_msg(recv_hdr,
+                      MACH_RCV_MSG,
+                      0,
+                      recv_hdr->msgh_size,
+                      client_port,
+                      MACH_MSG_TIMEOUT_NONE,
+                      MACH_PORT_NULL);
+        EXIT_ON_MACH_ERROR("mach_msg(recv)", kr, MACH_MSG_SUCCESS);
+        
+        printf("%d\n", recv_msg.data);
+    } while( recv_hdr->msgh_id != DEFAULT_MSG_ID);
+   
+    exit(0);
+}
---
+++ simple_ipc_common.h
@@ -0,0 +1,30 @@
+//
+//  simple_ipc_common.h
+//
+//  Copyright © 2016年 pebble8888. All rights reserved.
+//
+
+#ifndef simple_ipc_common_h
+#define simple_ipc_common_h
+
+#include <mach/mach.h>
+#include <servers/bootstrap.h>
+
+#define SERVICE_NAME "com.osxbook.FactorialServer"
+#define DEFAULT_MSG_ID 400
+
+#define EXIT_ON_MACH_ERROR(msg, retval, success_retval) \
+    if (kr != success_retval) { mach_error(msg ":", kr); exit((retval)); }
+
+typedef struct {
+    mach_msg_header_t header;
+    int               data;
+} msg_format_send_t;
+
+typedef struct {
+    mach_msg_header_t  header;
+    int                data;
+    mach_msg_trailer_t trailer; 
+} msg_format_recv_t;
+
+#endif /* simple_ipc_common_h */
